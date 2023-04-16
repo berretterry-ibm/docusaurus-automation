a1ba99e308ddd5a2378389246e822fb9ce2e868a
Berret Terry <berretterry@ibm.com>
2023-04-15 21:13:20 -0700

    added assets and updated build script

--- site_build.sh
+++ site_build.sh
@@ -16,4 +16,10 @@ rm -r website/docs
 
 mkdir wesite/docs
 
+cp assets/custom.css website/src/css/custom.css
+
+cp assets/docusaurus.config.js website/docusaurus.config.js
+
+cp assets/img website/static/img
+
 python3 mdsplit.py README.md --max-level 2 --force --verbose website/docs
